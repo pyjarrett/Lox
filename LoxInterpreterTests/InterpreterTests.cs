cb8680bdc72ea6593d2b4e43151b4e74beebb2fa
Paul Jarrett <jarrett.paul.young@gmail.com>
2023-10-26 20:15:32 -0400

    Clean up interpreter tests.

--- LoxInterpreterTests/InterpreterTests.cs
+++ LoxInterpreterTests/InterpreterTests.cs
@@ -8,47 +8,62 @@ namespace LoxInterpreterTests;
 public class InterpreterTests
 {
     [Fact]
-    public void Test1()
+    public void TestArithmetic()
     {
         VerifyEvaluation(7.0, "1 + 2 * 3");
         VerifyEvaluation(2.0, "1 + 8 * 2 / 4 - (3.0 * 1.0)");
+    }
 
+    [Fact]
+    public void TestComparisonOperators()
+    {
+        VerifyEvaluation(true, "1 != 2");
         VerifyEvaluation(true, "1 != 2");
         VerifyEvaluation(false, "1 == 2");
+        VerifyEvaluation(true, "1 == 1");
         VerifyEvaluation(true, "1 < 2");
         VerifyEvaluation(false, "2 < 1");
         VerifyEvaluation(true, "1 <= 2");
         VerifyEvaluation(true, "2 > 1");
         VerifyEvaluation(true, "2 >= 1");
 
+        VerifyEvaluation(true, "nil == nil");
+        VerifyEvaluation(false, "nil == 10");
+    }
+
+    [Fact]
+    public void TestUnaryEvaluation()
+    {
         VerifyEvaluation(10.0, "+10.0");
         VerifyEvaluation(-10.0, "-10.0");
         VerifyEvaluation(-20.0, "-(5 * 4)");
 
-        VerifyEvaluation(null, "nil");
-        VerifyEvaluation(true, "nil == nil");
-        VerifyEvaluation(false, "nil == 10");
         VerifyEvaluation(true, "!nil");
         VerifyEvaluation(false, "!!nil");
-
-        VerifyEvaluation(true, "true");
-        VerifyEvaluation(false, "false");
-
-        VerifyThrows(typeof(RuntimeError), "-true");
-        VerifyThrows(typeof(RuntimeError), "-false");
-        VerifyThrows(typeof(RuntimeError), "false + true");
-
-        VerifyThrows(typeof(ParseError), "(5.0");
-
         VerifyEvaluation(false, "!5.0");
     }
 
-    private void Evaluate(string text)
+    [Fact]
+    public void TestTruthiness()
     {
-        Lexer lexer = new Lexer(text);
-        Parser parser = new Parser(lexer.ScanTokens());
-        Interpreter interpreter = new Interpreter();
-        interpreter.Evaluate(parser.Expression());
+        VerifyEvaluation(null, "nil");
+
+        VerifyEvaluation(true, "true");
+        VerifyEvaluation(false, "false");
+    }
+
+    [Fact]
+    public void TestRuntimeErrors()
+    {
+        VerifyThrows(typeof(RuntimeError), "-true");
+        VerifyThrows(typeof(RuntimeError), "-false");
+        VerifyThrows(typeof(RuntimeError), "false + true");
+    }
+
+    [Fact]
+    public void TestParseError()
+    {
+        VerifyThrows(typeof(ParseError), "(5.0");
     }
 
     private void VerifyThrows(Type exception, string text)
